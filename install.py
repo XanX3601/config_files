d2dba81395fc1eca7edbdf8b8476e91b62ccd94e
Thomas Petiteau <thomas.petiteau@outlook.com>
2020-12-07 12:13:40 +0100

    Updated install.py

--- install.py
+++ install.py
@@ -1,4 +1,6 @@
 import click
+import git
+import pathlib
 import subprocess
 
 
@@ -9,10 +11,32 @@ def install():
 
 @install.command()
 def nvim():
+    home_directory = pathlib.Path.home()
+    nvim_repo_path = home_directory.joinpath('neovim')
+
+    if nvim_repo_path.exists():
+        try:
+            nvim_repo = git.Repo(nvim_repo_path)
+        except:
+            click.secho('{} already exists and can not be opened'.format(
+                nvim_repo_path),
+                        fg='red',
+                        err=True)
+    else:
+        nvim_repo = git.Repo.clone_from('https://github.com/neovim/neovim.git',
+                                        nvim_repo_path)
+
+    nvim_repo_build_path = nvim_repo_path.joinpath('build')
+
+    if not nvim_repo_build_path.exists():
+        nvim_repo_build_path.mkdir()
+
     args = [
-        'git', 'clone', 'https://github.com/neovim/neovim.git', '$HOME/neovim'
+        'cmake', '-S',
+        str(nvim_repo_path), '-B',
+        str(nvim_repo_build_path)
     ]
-    subprocess.run(args, shell=True, check=True).args
+    subprocess.run(args)
 
 
 @install.command()
